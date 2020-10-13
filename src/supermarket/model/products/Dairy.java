9433559e8a06d540db472786d174005423cd84d5
NikitaKolpakov27 <nik.kolpakov@inbox.ru>
2020-10-13 20:43:55 +0300

    Removed comments

--- src/supermarket/model/products/Dairy.java
+++ src/supermarket/model/products/Dairy.java
@@ -5,14 +5,6 @@ import supermarket.model.Types;
 import java.time.LocalDate;
 
 public class Dairy extends Product {
-
-//    public Dairy(int year, int month, int day, String name, String kind, double price) {
-//        this.shelfDate = LocalDate.of(year, month, day);
-//        this.name = name;
-//        this.price = price;
-//        this.type = Types.SINGLE_PIECE;
-//        this.kind = kind;
-//    }
     public Dairy(LocalDate date, String name, String kind, double price) {
         super(date, name, kind, price);
         this.type = Types.SINGLE_PIECE;
--- src/supermarket/model/products/Vegetables.java
+++ src/supermarket/model/products/Vegetables.java
@@ -5,15 +5,6 @@ import supermarket.model.Types;
 import java.time.LocalDate;
 
 public class Vegetables extends Product {
-
-//    public Vegetables(int year, int month, int day, String name, String kind, double price) {
-//        this.shelfDate = LocalDate.of(year, month, day);
-//        this.name = name;
-//        this.price = price;
-//        this.type = Types.WEIGHTY;
-//        this.kind = kind;
-//    }
-
     public Vegetables(LocalDate date, String name, String kind, double price) {
         super(date, name, kind, price);
         this.type = Types.WEIGHTY;
